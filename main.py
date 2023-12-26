97c80b0e7dab22c7a023a32a725ca04eeb56a1e3
vizkyy <62359058+vizkyy@users.noreply.github.com>
2023-12-26 22:43:17 +0000

    designed conversation menu, developed user assistance, placeholder options, exit option
     Design Conversation Menu: Design the structure for the conversation menu, including placeholder options.
    Develop User Assistance Query: Write the code for asking the user how the chatbot can assist them.
     Implement Placeholder Options: Code the placeholder options for the conversation, including their corresponding responses.
    Add Exit Option in Menu: Implement the option for the user to exit the conversation with an appropriate goodbye message.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-print("Hello! How am I assist you today?")
+print("Hello! How may I assist you today?")
 
-name = input("What is your name? ")
+name = input("Please enter your name: ") 
 
-age = input("What a beautiful name! How old are you? ")
+age = input("What a special name! How old are you? ")
+
+print("Oh what a wonderful age! What would you like to be assisted with today?")
+print("1. Placeholder Option 1 ")
+print("2. Placeholder Option 2 ")
+print("3. Placeholder Option 3 ")
+print("4. Exit Chatbot ")
+
+while True: 
+  choose_option = input("Please enter the number of choice 1-4: ")
+  
+  if choose_option == "1":
+    print("Placeholder Option 1")
+  elif choose_option == "2":
+    print("Placeholder Option 2")
+  elif choose_option == "3":
+    print("Placeholder Option 3")
+  elif choose_option == "4":
+    print(f"Thank you for using our chatbot, {name}! Have a great day!")
+    break
+  else:
+    print("Invalid option. Please try again.")
+  
