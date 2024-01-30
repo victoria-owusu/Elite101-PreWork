3cac6a3924ddb84042a877fc509e0dcd87e0facb
victoria-owusu <62359058+victoria-owusu@users.noreply.github.com>
2024-01-30 02:04:27 +0000

    revised chatbot to clothing store template

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-print("Hello! How may I assist you today?")
+print("Hello! I am StreetGlow's personal AI assistant. How may I assist you with a return or exchange?")
 
 name = input("Please enter your name: ") 
 
-age = input("What a special name! How old are you? ")
-
-print("Oh what a wonderful age! What would you like to be assisted with today?")
-print("1. Placeholder Option 1 ")
-print("2. Placeholder Option 2 ")
-print("3. Placeholder Option 3 ")
-print("4. Exit Chatbot ")
+print("What would you like to be assisted with today?")
+print("1. Returns")
+print("2. Exchanges")
+print("3. Exit Chatbot ")
 
 while True: 
-  choose_option = input("Please enter the number of choice 1-4: ")
+  choose_option = input("Please enter the number of choice 1-3: ")
   
   if choose_option == "1":
-    print("Placeholder Option 1")
+   print("How many days has it been since your last purchase?")
+   return_days = int(input("Please enter the number of days: "))
   elif choose_option == "2":
-    print("Placeholder Option 2")
+    exchange_days = print("How many days has it been since your last purchase?")
   elif choose_option == "3":
-    print("Placeholder Option 3")
-  elif choose_option == "4":
     print(f"Thank you for using our chatbot, {name}! Have a great day!")
     break
   else:
